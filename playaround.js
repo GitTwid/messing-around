f48208612c8a176f9788616b80c5ef86f7497b7b
Patrick Draper <patrick.draper@wrenliving.com>
2016-06-06 11:59:58 +0100

    Update js file

---
+++ playaround.js
@@ -0,0 +1,17 @@
+var fn = "Philip";
+var sn = "Twidle";
+var dob = "14/01/1993";
+var ad1 = "55 Dam Road";
+var ad2 = "Barton-upon-Humber";
+var pc = "DN18 5BT";
+var oc = "Junior Developer";
+
+document.write(string["Full name: " + fn + " " + sn]);
+document.write(string["<br /><br />"]);
+document.write(string["Address Line 1: " + ad1]);
+document.write(string["<br />"]);
+document.write(string["Address Line 2: " + ad2]);
+document.write(string["<br />"]);
+document.write(string["Postcode: " + pc]);
+document.write(string["<br /><br />"]);
+document.write(string["Occupation: " + oc]);
